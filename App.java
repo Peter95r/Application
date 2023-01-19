a1878dd70bde43b38dc5c899de830ed34349059c
Roger Wolf <roger.a.wolf@hotmail.nl>
2023-01-19 18:38:48 +0100

    work on warehouse menu

--- App.java
+++ App.java
@@ -7,7 +7,7 @@ import Application.cargo.*;
 public class App {
     public static void main(String[] args){
         ArrayList<Warehouse> warehouseList = new ArrayList<>();     //Instantiation of arraylist in which all warehouses can be stored
-        Scanner scan = new Scanner(System.in);                      //Instantiate scanner
+        Scanner scan = new Scanner(System.in);                      //Instantiation of scanner
         Warehouse currentWarehouse;                                 //Declare variable to store currently selected warehouse
 
         //Instantiation of several warehouses, to be added to warehouseList
@@ -18,40 +18,49 @@ public class App {
         currentWarehouse = warehouseSelection(warehouseList, scan);
         System.out.println(currentWarehouse);
 
-
+        currentWarehouse.selectOption();
     }
 
+    public void selectOption(){
 
+
+        while(true){
+            System.out.println("What would you like to do in this warehouse?");
+            System.out.println("1: Load cargo into warehouse");
+            System.out.println("2: Move cargo from this warehouse to another");
+            System.out.println("3: Return to warehouse selection");
+            System.out.print("Please select an option: ");
+
+            try {
+
+            } catch (Exception e) {
+                System.out.println("Incorrect input, please select a warehouse by entering a valid option.\n");
+            }
+        }
+    }
 
 
 
     //Created a static method for warehouse selection
     public static Warehouse warehouseSelection(ArrayList<Warehouse> warehouseList, Scanner scan){
         while(true){
-            int chosenIndex = 0;
-
             System.out.println("List of warehouses: ");
+
             //Print an option and the name (city) for each warehouse
             for(Warehouse warehouse: warehouseList){
                 System.out.println((warehouseList.indexOf(warehouse) + 1) + ": " + warehouse.getName());
             }
 
-            System.out.print("Select warehouse: ");
-            //Ask for user input
-            String input = scan.nextLine();
+            System.out.print("Select warehouse: ");         //...
 
-            //check whether user gives an Integer input. If so, store in chosenIndex.
+            String input = scan.nextLine();                 //Ask for user input
+
+            //Check whether user gives a valid option as input. If so, return the Warehouse object corresponding to
+            //that option. If not, retry.
             try {
-                chosenIndex = Integer.valueOf(input) - 1;
-            } catch(Exception e){
-                //possible error message here, although it is already offered in the next if-block as well. Probably unnecessary
-            }
-
-            if(chosenIndex >= 1 && chosenIndex <= warehouseList.size())  {
-                return warehouseList.get(chosenIndex);
-
-            } else {
-                System.out.println("Incorrect input, please select a warehouse by entering a valid option.");
+                return warehouseList.get(Integer.parseInt(input)-1);
+            } catch(Exception e) {
+                System.out.println("Incorrect input, please select a warehouse by entering a valid option.\n");
             }
         }
     }

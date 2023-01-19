e9dce76303360ef54537cc41a18a03442eea599c
Piotr Zawislak <p.zawislak@workingspirit.nl>
2023-01-19 13:00:37 +0100

    new methods

--- App.java
+++ App.java
@@ -1,6 +1,4 @@
 package Application;
-import java.net.StandardSocketOptions;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -28,7 +26,42 @@ public class App {
 
 class Warehouse{
 	
-	int capasity = 1000;
-	Cargo cargo;
+	int maxCapacity = 1000; // explanation no needed
+	int currentCapacity;    // max capacity - cargo.amount its for calculation to tell if we have enough space of new cargo or not
+	
+	Cargo wood;
+	Cargo iron;
+	Cargo lego;
+	
+	
+	public void fillWarehouse(int add) {
+		// @@ "I have limit 1000 tons"
+		// @@ "I want to choose what cargo is coming in: wood, iron, lego"
+		// @@ "Add some amount of choosen cargo"
+		// @@ "Check if i have enough space"
+		// @@ "if no, change amount"             if capacity - cargo.amount < 0;
+		// @@ "if yes, add the amount of cargo"  if capacity - cargo.amount >=0;
+		// @@ "tell me how much capacity i have left"
+		// @@ propably: capacity - wood.amount = space left;
+		// @@            1000    -    wood.320 =    680;
+		// @@            capacity = 680;
+	}
+	
+	public void sellCargo() {
+		// @@ "choose which cargo has to go"
+		// @@ "tell me how much we are goint to sell"
+		// @@ "i will check if we have this amount"
+		// @@ "if not: you can call method fillWarehouse or change amount of this cargo "
+		// @@ "choose transport"
+		// @@ "i will check if transport can carry this cargo"
+		// @@ "if not: back to change amount step or change transport"
+		// @@ "if everything is all right: send cargo and display message"
+		// @@ "update the data: for example wood.amount - sended amount = updated wood.amount
+		// @@ updated currentCapacity = capacity - updated wood.amount"
+	}
+	
+	public void sendCargo() {
+		
+	}
 	
 }

a494f5d40cd210b56e926a880b566c0a4f0617f2
Roger Wolf <roger.a.wolf@hotmail.nl>
2023-01-19 21:29:12 +0100

    added menu structure, contingencies for wrong inputs, a custom Exception and made loading available in the warehouse

--- App.java
+++ App.java
@@ -1,4 +1,5 @@
 package Application;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
 import Application.cargo.*;
@@ -6,52 +7,68 @@ import Application.cargo.*;
 
 public class App {
     public static void main(String[] args){
-    	
-    	// This is only for fill methodTest: run it so you know how it looks 
-    	Warehouse ww = new Warehouse("TestHouse");
-    	ww.fillWarehouse();
-    	ww.fillWarehouse();
-    	ww.fillWarehouse();
-    	
         ArrayList<Warehouse> warehouseList = new ArrayList<>();     //Instantiation of arraylist in which all warehouses can be stored
-        Scanner scan = new Scanner(System.in);                      //Instantiation of scanner
-        Warehouse currentWarehouse;                                 //Declare variable to store currently selected warehouse
+        Warehouse selectedWarehouse;                                 //Declare variable to store currently selected warehouse
 
         //Instantiation of several warehouses, to be added to warehouseList
         warehouseList.add(new Warehouse("Deventer"));
         warehouseList.add(new Warehouse("Berlin"));
         warehouseList.add(new Warehouse("Warszawa"));
 
-        currentWarehouse = warehouseSelection(warehouseList, scan);
-        System.out.println(currentWarehouse);
+        //Call function to select a warehouse and store the chosen warehouse in variable
+        while(true){
+            selectedWarehouse = warehouseSelection(warehouseList);
+            selectWarehouseOption(selectedWarehouse);
+        }
 
-        currentWarehouse.selectOption();
+
+
     }
 
-    public void selectOption(){
+    public static void selectWarehouseOption(Warehouse selectedWarehouse){
+        Scanner scan = new Scanner(System.in);                      //Instantiation of scanner
+        outer: while(true){
+            System.out.println(selectedWarehouse);
 
-
-        while(true){
-            System.out.println("What would you like to do in this warehouse?");
+            System.out.println("\nWhat would you like to do in this warehouse?");
             System.out.println("1: Load cargo into warehouse");
             System.out.println("2: Move cargo from this warehouse to another");
-            System.out.println("3: Return to warehouse selection");
+            System.out.println("3: Sell cargo");
+            System.out.println("4: Return to warehouse selection");
             System.out.print("Please select an option: ");
 
-            try {
+            String input = scan.nextLine();
 
-            } catch (Exception e) {
-                System.out.println("Incorrect input, please select a warehouse by entering a valid option.\n");
+            try {
+                if(Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 4){
+                    switch(input){
+                        case "1":
+                            selectedWarehouse.fillWarehouse();
+                            break;
+                        case "2":
+                            selectedWarehouse.sendCargo();
+                            break;
+                        case "3":
+                            selectedWarehouse.sellCargo();
+                            break;
+                        case "4":
+                            break outer;
+                    }
+                } else {
+                    throw new InvalidSelectionException();
+                }
+            } catch (Exception e){
+                System.out.println("\nIncorrect input, please select a valid option.");
             }
         }
     }
 
-
-
     //Created a static method for warehouse selection
-    public static Warehouse warehouseSelection(ArrayList<Warehouse> warehouseList, Scanner scan){
+    public static Warehouse warehouseSelection(ArrayList<Warehouse> warehouseList){
+        Scanner scan = new Scanner(System.in);                      //Instantiation of scanner
+
         while(true){
-            System.out.println("List of warehouses: ");
+            System.out.println("\nList of warehouses: ");
 
             //Print an option and the name (city) for each warehouse
             for(Warehouse warehouse: warehouseList){
@@ -62,12 +79,12 @@ public class App {
 
             String input = scan.nextLine();                 //Ask for user input
 
-            //Check whether user gives a valid option as input. If so, return the Warehouse object corresponding to
+            //Check whether user gives a valid option (between 1 and warehouseList.size() as input. If so, return the Warehouse object corresponding to
             //that option. If not, retry.
             try {
                 return warehouseList.get(Integer.parseInt(input)-1);
             } catch(Exception e) {
-                System.out.println("Incorrect input, please select a warehouse by entering a valid option.\n");
+                System.out.println("\nIncorrect input, please select a valid option.");
             }
         }
     }
---
+++ InvalidSelectionException.java
@@ -0,0 +1,5 @@
+package Application;
+
+public class InvalidSelectionException extends Exception {
+
+}

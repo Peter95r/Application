bdfb4a09c394b1c487800e765da760e0e6572e68
Piotr Zawislak <p.zawislak@workingspirit.nl>
2023-01-20 13:47:57 +0100

    bla bla

--- cargo/Lego.java
+++ cargo/Lego.java
@@ -3,7 +3,7 @@ package Application.cargo;
 public class Lego implements Cargo{
 
 	private int weight =0;                         //weight in tones
-	static final double buyValue = 70, sellValue = 115;        
+	public  static final double buyValue = 70, sellValue = 115;        
 	
 	public int getWeight() {return this.weight;}
 	
--- cargo/Wood.java
+++ cargo/Wood.java
@@ -3,7 +3,7 @@ package Application.cargo;
 public class Wood implements Cargo{
 
 	private int weight =0;                 //weight in tones
-	static final double buyValue = 40, sellValue = 65;     
+	public static final double buyValue = 40, sellValue = 65;     
 	
 	
 	public int getWeight() {return this.weight;}
